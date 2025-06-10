ea8d2ce7e64d5aebfae93b7ad3ef0865f3333296
MikaColt <mcrisafi@live.com>
2025-06-10 14:32:28 -0400

    6/10/2025 2:30pm
    Changes Made to MultiCoord. Check to see if terrain object works correctly.

--- MultiDTowerDefense/Assets/Scripts/Coordinates/MultiCoord.cs
+++ MultiDTowerDefense/Assets/Scripts/Coordinates/MultiCoord.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class MultiCoord 
 {
+    private Vector3 CurrentCoord 
+    {
+        get { return new Vector3(0, 0, 0); }
+        set { }
+    }
+    private Vector3Int CurrentOrientation { get { return new Vector3(0, 0, 0); } set; }
 
+    private int Length {  get { return 0; } }
 }
