480e81985e36a5b5c81021d4c91860fb6316ee18
MikaColt <mcrisafi@live.com>
2025-06-10 14:42:03 -0400

    6/10/2025 2:40pm
    Modified MultiCoord class

--- MultiDTowerDefense/Assets/Scripts/Coordinates/MultiCoord.cs
+++ MultiDTowerDefense/Assets/Scripts/Coordinates/MultiCoord.cs
@@ -9,7 +9,7 @@ public class MultiCoord
         get { return new Vector3(0, 0, 0); }
         set { }
     }
-    private Vector3Int CurrentOrientation { get { return new Vector3(0, 0, 0); } set; }
+    private Vector3Int CurrentOrientation { get { return new Vector3Int(0, 0, 0); } set { } }
 
     private int Length {  get { return 0; } }
 }
